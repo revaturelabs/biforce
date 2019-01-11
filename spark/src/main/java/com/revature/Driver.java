9531b6bb52a000cf9f8e95b6ffcfa25df6f14c1f
Dela Anthonio <dell.anthonio@gmail.com>
2019-01-11 15:30:14 -0500

    [spark] Add Support for Setting the Master
    Resolve #33.

--- spark/src/main/java/com/revature/Driver.java
+++ spark/src/main/java/com/revature/Driver.java
@@ -9,18 +9,18 @@ import org.apache.spark.sql.SparkSession;
 public class Driver {
 
   public static void main(String[] args) {
-    if (args.length != 2) {
-      System.err.println("Usage: <input file> <output dir>");
+    if (args.length != 3) {
+      System.err.println("Usage: <spark_master> <input_file> <output_dir>");
       System.exit(1);
     }
-
-    final String inputPath = args[0];
-    final String outputPath = args[1];
+    final String master = args[0];
+    final String inputPath = args[1];
+    final String outputPath = args[2];
 
     SparkSession spark = SparkSession
         .builder()
         .appName("Battery Analyzer")
-        .master("yarn")
+        .master(master)
         .getOrCreate();
 
     new BatteryTestRegression().execute(spark, inputPath, outputPath);
