4d4a445a6343c63274de5c93bbf20b7804b6f7f7
Dela Anthonio <dell.anthonio@gmail.com>
2019-01-15 10:56:40 -0500

    [spark] Remove Requirement for Specifying Spark Master

--- spark/src/main/java/com/revature/MLDriver.java
+++ spark/src/main/java/com/revature/MLDriver.java
@@ -9,18 +9,18 @@ import org.apache.spark.sql.SparkSession;
 public class MLDriver {
 
   public static void main(String[] args) {
-    if (args.length != 3) {
-      System.err.println("Usage: <spark_master> <input_file> <output_dir>");
+    if (args.length < 2) {
+      System.err.println("Usage: <input_file> <output_dir>");
       System.exit(1);
     }
-    final String master = args[0];
-    final String inputPath = args[1];
-    final String outputPath = args[2];
+
+    final String inputPath = args[0];
+    final String outputPath = args[1];
 
     SparkSession spark = SparkSession
         .builder()
         .appName("Battery Analyzer")
-        .master(master)
+        .master("local[*]")
         .getOrCreate();
 
     new BatterySuccessPredictor().execute(spark, inputPath, outputPath);
