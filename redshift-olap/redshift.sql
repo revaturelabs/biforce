4bfcde0677cdc82fad3072525f3f75a4ecb62960
averyehnis <44790664+averyehnis@users.noreply.github.com>
2019-01-16 10:32:30 -0500

    Update and rename RedShift table create and copy.sql to redshift.sql

--- redshift-olap/redshift.sql
+++ redshift-olap/redshift.sql
@@ -3,16 +3,16 @@ builder_id integer, group_id integer, group_type integer, battery_id integer, ba
 
 copy battery_test
 from 's3://revature-analytics-dev/battery_test.csv'
-access_key_id 'AKIAICKVX3BFQL4IT4RQ'
-secret_access_key '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'
+access_key_id '<S3 access key>'
+secret_access_key '<S3 secret key>'
 format as csv;
 
 create table if not exists ml_output(battery_id integer not null, pass_fail decimal not null);
 
 copy ml_output
 from 's3://revature-analytics-dev/part-00000-10a17d42-4a75-461e-a93d-7b1fc9b8b269-c000.csv'
-access_key_id 'AKIAICKVX3BFQL4IT4RQ'
-secret_access_key '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'
+access_key_id '<S3 access key>'
+secret_access_key '<S3 secret key>'
 format as csv;
 
 create table if not exists spark_output(battery_id integer not null, fail_chance real not null,
@@ -20,6 +20,6 @@ sample_size integer);
 
 copy ml_output
 from 's3://revature-analytics-dev/FinalOutput.csv'
-access_key_id 'AKIAICKVX3BFQL4IT4RQ'
-secret_access_key 'qY1UIJmJozxv9mY13xAySq/iaSbecnqFnito9uXw'
+access_key_id '<S3 access key>'
+secret_access_key '<S3 secret key>'
 format as csv;
