a24fa500b4338ecbcbd6c5852e70eb5fda78a8ab
Mason <masonwegert@gmail.com>
2019-02-13 10:29:47 -0500

    Performance optimizations for pseudo code complete.

--- biforce/spark/BatteryAnalysis/src/main/java/com/revature/Driver.java
+++ biforce/spark/BatteryAnalysis/src/main/java/com/revature/Driver.java
@@ -7,60 +7,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;;
+import org.apache.spark.sql.SparkSession;
+
+/*
+ * _c0 = PK
+ * _c1 = test type
+ * _c3 = score
+ * _c4 = test period
+ * _c9 = associate id
+ * _c10 = associate status
+ */
+
+// Read the input file in as a spark Dataset<Row> with no header, therefore the
+// resulting table column names are in the format _c#.
 
 public class Driver {
 
 	private static BufferedWriter writer;
-	private static Dataset<Row> csv,filtered_csv,controlData,modelData;
-	private static int count = 0;
 	private static JavaSparkContext context;
-	
-	/*
-	 * Creates the output file, filters the data to just the relevant values,
-	 * then runs the tests on each unique id.
-	 */
-	public static void main(String args[]) {
-		
-		double[] splitRatios = {0.8,0.2};
+	public static Dataset<Row> csv,filtered_csv,controlData,modelData;
+	private static SparkSession session;
+	private static SparkConf conf;
 
+	public static void main(String args[]) {
+		double[] splitRatios = {0.8,0.2};
 		try {
 			writer = new BufferedWriter(new FileWriter(args[1], true));
 			writer.append("battery_id,% Chance to Fail\n");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-		// Set Spark configuration for Context.
-		SparkConf conf = new SparkConf().setAppName("ChanceToFail");
+		conf = new SparkConf().setAppName("ChanceToFail");
 		context = new JavaSparkContext(conf);
 		context.setLogLevel("ERROR");
-		SparkSession session = new SparkSession(context.sc());
-		
-		/*
-		 * Read in the data from the input file
-		 * _c0 = PK
-		 * _c1 = test type
-		 * _c3 = score
-		 * _c4 = test period
-		 * _c9 = associate id
-		 * _c10 = associate status
-		 */
-
-		// Read the input file in as a spark Dataset<Row> with no header, therefore the
-		// resulting table column names are in the format _c#.
+		session = new SparkSession(context.sc());
 		csv = session.read().format("csv").option("header","false").load(args[0]);
-
+		
+		csv.cache();
+		
 		// Filter the indicator data to include only the valid data for our samples.
 		filtered_csv = csv.filter("_c10 = 0 OR _c10 = 1 OR (_c10 = 2 AND (_c4 = 9 OR _c4 = 10))");
-		// Dataset<Row> testCounts = filtered_csv.groupBy("_c9").count();
-		// filtered_csv.javaRDD().filter(row->row.getInt(x) > 2); //do something like this for filter >2 tests
-		
+
+		//		Dataset<Row> testCounts = filtered_csv.groupBy("_c9").count();
+		//		filtered_csv.javaRDD().filter(row->row.getInt(x) > 2); //do something like this for filter >2 tests
+
 		Dataset<Row>[] splits = filtered_csv.randomSplit(splitRatios);
 		modelData = splits[0];
 		controlData = splits[1];
@@ -68,12 +61,11 @@ public class Driver {
 		// Build a logarithmic model with modeldata each test
 		/* xyz */
 		// model built
-
 		//apply model to each row
-		performTestingOnRows(csv);
-		
+		performTestingOnRows();
+
 		// use controlData to test accuracy
-		
+
 		// Close all the resources.
 		try {
 			writer.close();
@@ -85,53 +77,40 @@ public class Driver {
 	}
 
 	// This method writes output to file based on 'csv'
-	public static void performTestingOnRows(Dataset<Row> data) {
-		JavaRDD<Row> associateIDs = data.drop("_c0", "_c1","_c2","_c3","_c4","_c5","_c6","_c7","_c8","_c10").dropDuplicates().javaRDD();
-		// We have a class that takes a test # 0-6 and outputs a % chance of being dropped
-		// We also have to retrieve r^2's from this class, weight the drop %, and combine
-		associateIDs.map(new FindTestScore());
-	}
-	
-	
-	private static class FindTestScore implements Function<Row,Double>{
-		private static final long serialVersionUID = -3035931770787238206L;
+	public static void performTestingOnRows() {
+		// get rid of .orderBy if running slowly
+		List<Row> IDs = csv.drop("_c0", "_c1","_c2","_c3","_c4","_c5","_c6","_c7","_c8","_c10").dropDuplicates().orderBy("_c9").collectAsList();
 
-		public Double call(Row r){
-			double finalPercentage = 0;
-			long associateID = r.getLong(0);
-			System.out.println(count + ". Beginning Analysis on battery id: " + associateID);
-			context.sc().log().info(count + ". Beginning Analysis on battery id: " + associateID);
-			
-			List<Double> dropChances = new ArrayList<>();
-			List<Double> correlations = new ArrayList<>();
-			List<Row> associateTests = csv.where("_c9=" + associateID).collectAsList();
-			
+		for (Row r:IDs) {
+			Double finalPercentage = 0.0;
+			long associateID = Long.parseLong(r.getString(0));
+
+			System.out.println("Beginning Analysis on battery id: " + associateID);
+			context.sc().log().info("Beginning Analysis on battery id: " + associateID);
+
+			List<Row> associateTests = new ArrayList<Row>();
+
+			associateTests = csv.where("_c9=" + associateID).collectAsList();
+
 			double total_r = 0;
 			for (Row row:associateTests) {
 				//test type: row.getInt(1)
 				//week number: row.getInt(4)
 				//test score: row.getDouble(3)
-				/*
-				 * Use class method to grab %
-				 */
 				row.get(1);
-				dropChances.add(5.0);
-				correlations.add(0.7);
+
+				finalPercentage += Double.parseDouble(row.getString(3))*0.7;
 				total_r += 0.7;
 			}
-			
-			for (int i = 0;i<dropChances.size();++i) {
-				finalPercentage += dropChances.get(i)*correlations.get(i)/total_r;
-			}
-			
+			finalPercentage /= total_r;
+
 			try {
 				writer.append(associateID + "," + finalPercentage + '\n');
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			context.sc().log().info("Aggregated Result: Battery_id: " + associateID + ", % Chance to fail: " + finalPercentage);
-			System.out.println("Aggregated Result: Battery_id: " + associateID + ", % Chance to fail: " + finalPercentage);
-			return finalPercentage;
+			context.sc().log().info("> Aggregated Result: Battery_id: " + associateID + ", % Chance to fail: " + finalPercentage);
+			System.out.println("> Aggregated Result: Battery_id: " + associateID + ", % Chance to fail: " + finalPercentage);
 		}
 	}
 }
--- biforce/spark/BatteryAnalysis/src/main/java/com/revature/Driver_old.java
+++ biforce/spark/BatteryAnalysis/src/main/java/com/revature/Driver_old.java
@@ -1,166 +1,166 @@
-package com.revature;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
-
-import com.revature.spark.AnalyticResult;
-import com.revature.spark.PassFailSampleFilter;
-import static com.revature.spark.TestIndicator.execute;;
-
-public class Driver_old {
-	
-	private static List<AnalyticResult> results;
-	private static BufferedWriter writer;
-	private static Dataset<Row> csv,filtered_csv;
-	private static int count = 0;
-	private static JavaSparkContext context;
-	
-	/*
-	 * Creates the output file, filters the data to just the relevant values,
-	 * then runs the tests on each unique id.
-	 */
-	public static void main(String args[]) {
-		
-		//final String SPARK_MASTER = args[0];
-		final String INPUT_PATH = args[0];
-		final String OUTPUT_PATH = args[1];
-		int TARGET_BATCH = 0;
-		try {
-			if (args.length==4)
-				TARGET_BATCH = Integer.parseInt(args[3]);
-		}
-		catch (NumberFormatException e) {
-			System.out.println("4th argument must be an integer");
-			System.exit(1);
-		}
-		
-		try {
-			writer = new BufferedWriter(new FileWriter(OUTPUT_PATH, true));
-			writer.append("battery_id,% Chance to Fail, Total Sample Size\n");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	
-		// Set Spark configuration for Context.
-		//SparkConf conf = new SparkConf().setAppName("ChanceToFail").setMaster(SPARK_MASTER);
-		SparkConf conf = new SparkConf().setAppName("ChanceToFail");
-		context = new JavaSparkContext(conf);
-		context.setLogLevel("ERROR");
-		SparkSession session = new SparkSession(context.sc());
-		results = new ArrayList<AnalyticResult>();
-		
-		/*
-		 * Read in the data from the input file
-		 * _c1 = test type
-		 * _c2 = raw score
-		 * _c3 = score
-		 * _c4 = test period
-		 * _c5 = test category
-		 * _c6 = builder id
-		 * _c7 = group id
-		 * _c8 = group type
-		 * _c9 = battery id
-		 * _c10 = battery status
-		 */
-		
-		// Read the input file in as a spark Dataset<Row> with no header, therefore the
-		// resulting table column names are in the format _c#.
-		csv = session.read().format("csv").option("header","false").load(INPUT_PATH);
-		
-		// Create a list containing each row with battery id as a primary key.
-		Dataset<Row> uniqueBatteries;
-		if (args.length==4)
-			uniqueBatteries = csv.filter("_c7 = " + TARGET_BATCH).groupBy("_c9").count();
-		else
-			uniqueBatteries = csv.groupBy("_c9").count();
-			
-		
-		List<Row> rowList = uniqueBatteries.toJavaRDD().collect();
-		
-		// Filter the indicator data to include only the valid data for our samples.
-		filtered_csv = PassFailSampleFilter.execute(csv);
-		
-		// Run the tests for each battery id.
-		int counter = 0;
-		for (Row row : rowList) {
-			performTestingOnRows(csv.filter("_c9 = " + row.get(0).toString()));
-			if(counter>2) break;
-			counter++;
-		}
-		
-		// Close all the resources.
-		try {
-			writer.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		session.close();
-		context.close();
-	}
-	
-	/**
-	 * Call the various indicator tests and add the results to our result list.
-	 * Consists of looking at the first 3 tests in the first 3 periods.
-	 */
-	public static void performTestingOnRows(Dataset<Row> battery_id_tests) {
-		int totalSampleSize;
-		double finalPercentage;
-		AnalyticResult newResult;
-		int input_battery_id = Integer.parseInt(battery_id_tests.first().getString(8).toString());
-			
-		System.out.println(count + ". Beginning Analysis on battery id: " + input_battery_id);
-		context.sc().log().info(count + ". Beginning Analysis on battery id: " + input_battery_id);
-		
-			for (int i = 1;i<4;i++)
-				for (int j = 1;j<4; j++) {
-					newResult = execute(filtered_csv,battery_id_tests,i,j);
-					System.gc();
-					results.add(newResult);
-					if (newResult!=null) {
-						context.sc().log().info(newResult.toString());
-						System.out.println(newResult);
-					}
-				}
-			
-			// Sum up the total of the sample sizes for each result.
-			totalSampleSize = 0;
-			for (AnalyticResult result:results) {
-				if (result!=null) {
-					totalSampleSize+=result.getSampleSize();
-				}
-			}
-			
-			// Use the sample size sum and calculate the final percentage by weighing each result by their sample size.
-			finalPercentage = 0;
-			for (AnalyticResult result:results) {
-				if (result!=null) {
-					finalPercentage += result.getPercentage() * (result.getSampleSize()/(double)totalSampleSize);
-				}
-			}
-			
-			// Log the results.
-			context.sc().log().info("Aggregated Result: Battery_id: " + input_battery_id + ", % Chance to fail: " + finalPercentage + ", Total Sample Size: " + totalSampleSize);
-			System.out.println("Aggregated Result: Battery_id: " + input_battery_id + ", % Chance to fail: " + finalPercentage + ", Total Sample Size: " + totalSampleSize);
-			
-			// Append the results to the output file.
-			try {
-				writer.append(input_battery_id + "," + finalPercentage + "," + totalSampleSize+'\n');
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-			count++;
-			
-			// Clear the results of these tests to make way for the next battery id.
-			results.clear();
-	}
-}
+//package com.revature;
+//
+//import java.io.BufferedWriter;
+//import java.io.FileWriter;
+//import java.io.IOException;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import org.apache.spark.SparkConf;
+//import org.apache.spark.api.java.JavaSparkContext;
+//import org.apache.spark.sql.Dataset;
+//import org.apache.spark.sql.Row;
+//import org.apache.spark.sql.SparkSession;
+//
+//import com.revature.spark.AnalyticResult;
+//import com.revature.spark.PassFailSampleFilter;
+//import static com.revature.spark.TestIndicator.execute;;
+//
+//public class Driver_old {
+//	
+//	private static List<AnalyticResult> results;
+//	private static BufferedWriter writer;
+//	private static Dataset<Row> csv,filtered_csv;
+//	private static int count = 0;
+//	private static JavaSparkContext context;
+//	
+//	/*
+//	 * Creates the output file, filters the data to just the relevant values,
+//	 * then runs the tests on each unique id.
+//	 */
+//	public static void main(String args[]) {
+//		
+//		//final String SPARK_MASTER = args[0];
+//		final String INPUT_PATH = args[0];
+//		final String OUTPUT_PATH = args[1];
+//		int TARGET_BATCH = 0;
+//		try {
+//			if (args.length==4)
+//				TARGET_BATCH = Integer.parseInt(args[3]);
+//		}
+//		catch (NumberFormatException e) {
+//			System.out.println("4th argument must be an integer");
+//			System.exit(1);
+//		}
+//		
+//		try {
+//			writer = new BufferedWriter(new FileWriter(OUTPUT_PATH, true));
+//			writer.append("battery_id,% Chance to Fail, Total Sample Size\n");
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	
+//		// Set Spark configuration for Context.
+//		//SparkConf conf = new SparkConf().setAppName("ChanceToFail").setMaster(SPARK_MASTER);
+//		SparkConf conf = new SparkConf().setAppName("ChanceToFail");
+//		context = new JavaSparkContext(conf);
+//		context.setLogLevel("ERROR");
+//		SparkSession session = new SparkSession(context.sc());
+//		results = new ArrayList<AnalyticResult>();
+//		
+//		/*
+//		 * Read in the data from the input file
+//		 * _c1 = test type
+//		 * _c2 = raw score
+//		 * _c3 = score
+//		 * _c4 = test period
+//		 * _c5 = test category
+//		 * _c6 = builder id
+//		 * _c7 = group id
+//		 * _c8 = group type
+//		 * _c9 = battery id
+//		 * _c10 = battery status
+//		 */
+//		
+//		// Read the input file in as a spark Dataset<Row> with no header, therefore the
+//		// resulting table column names are in the format _c#.
+//		csv = session.read().format("csv").option("header","false").load(INPUT_PATH);
+//		
+//		// Create a list containing each row with battery id as a primary key.
+//		Dataset<Row> uniqueBatteries;
+//		if (args.length==4)
+//			uniqueBatteries = csv.filter("_c7 = " + TARGET_BATCH).groupBy("_c9").count();
+//		else
+//			uniqueBatteries = csv.groupBy("_c9").count();
+//			
+//		
+//		List<Row> rowList = uniqueBatteries.toJavaRDD().collect();
+//		
+//		// Filter the indicator data to include only the valid data for our samples.
+//		filtered_csv = PassFailSampleFilter.execute(csv);
+//		
+//		// Run the tests for each battery id.
+//		int counter = 0;
+//		for (Row row : rowList) {
+//			performTestingOnRows(csv.filter("_c9 = " + row.get(0).toString()));
+//			if(counter>2) break;
+//			counter++;
+//		}
+//		
+//		// Close all the resources.
+//		try {
+//			writer.close();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		session.close();
+//		context.close();
+//	}
+//	
+//	/**
+//	 * Call the various indicator tests and add the results to our result list.
+//	 * Consists of looking at the first 3 tests in the first 3 periods.
+//	 */
+//	public static void performTestingOnRows(Dataset<Row> battery_id_tests) {
+//		int totalSampleSize;
+//		double finalPercentage;
+//		AnalyticResult newResult;
+//		int input_battery_id = Integer.parseInt(battery_id_tests.first().getString(8).toString());
+//			
+//		System.out.println(count + ". Beginning Analysis on battery id: " + input_battery_id);
+//		context.sc().log().info(count + ". Beginning Analysis on battery id: " + input_battery_id);
+//		
+//			for (int i = 1;i<4;i++)
+//				for (int j = 1;j<4; j++) {
+//					newResult = execute(filtered_csv,battery_id_tests,i,j);
+//					System.gc();
+//					results.add(newResult);
+//					if (newResult!=null) {
+//						context.sc().log().info(newResult.toString());
+//						System.out.println(newResult);
+//					}
+//				}
+//			
+//			// Sum up the total of the sample sizes for each result.
+//			totalSampleSize = 0;
+//			for (AnalyticResult result:results) {
+//				if (result!=null) {
+//					totalSampleSize+=result.getSampleSize();
+//				}
+//			}
+//			
+//			// Use the sample size sum and calculate the final percentage by weighing each result by their sample size.
+//			finalPercentage = 0;
+//			for (AnalyticResult result:results) {
+//				if (result!=null) {
+//					finalPercentage += result.getPercentage() * (result.getSampleSize()/(double)totalSampleSize);
+//				}
+//			}
+//			
+//			// Log the results.
+//			context.sc().log().info("Aggregated Result: Battery_id: " + input_battery_id + ", % Chance to fail: " + finalPercentage + ", Total Sample Size: " + totalSampleSize);
+//			System.out.println("Aggregated Result: Battery_id: " + input_battery_id + ", % Chance to fail: " + finalPercentage + ", Total Sample Size: " + totalSampleSize);
+//			
+//			// Append the results to the output file.
+//			try {
+//				writer.append(input_battery_id + "," + finalPercentage + "," + totalSampleSize+'\n');
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//			
+//			count++;
+//			
+//			// Clear the results of these tests to make way for the next battery id.
+//			results.clear();
+//	}
+//}
