8ec8c25cad42b26bb5c37221a2ca19aa89685e29
Pil Ju Chun <chunpilju@gmail.com>
2019-02-22 14:43:12 -0500

    Cleaned up Driver code
    Moved output print statement to method & created javadoc

--- biforce/spark/BatteryAnalysis/src/main/java/com/revature/Driver.java
+++ biforce/spark/BatteryAnalysis/src/main/java/com/revature/Driver.java
@@ -19,23 +19,37 @@ import com.revature.util.OptimalPoint;
 import com.revature.util.PartitionFinder;
 
 /**
- * <h1> Biforce Spark Team JavaDoc </h1>
- * <p>This class consists exclusively of a main method that interacts with utilities
- * which interact with a logistic regression model. It has several helper methods
- * which write output. All methods are static.</p>
- * <p>The model returns percentage values which indicate likelihood to fail and then
- * runs through all possible cutoff values to determine the optimal cutoff percentage. </p>
- * <p>The model clumps trainees together in groups for each of the first few weeks by
- * their test scores into buckets based on equi-distant percentiles. The model then
- * calculates the odds of someone in that bucket passing or failing and then calculates
- * the log odds taken from this tutorial. <a href="http://vassarstats.net/logreg1.html">Logistic Regression</a>.</p>
- * <p>TODO: describe the rest of log-reg logic.</p>
- * <p>This class is a member of the
- * <a href="https://github.com/Dec-17-Big-Data/biforce">
- * Revature Biforce</a> Github project.</p>
- * <p>For more information regarding this project see the
- * <a href="https://drive.google.com/open?id=1xD-x-0oX2vXWdEpoMhq0Jpu0j2gCgpjOufVh5NerJHQ">
- * Biforce Living Document</a>.</p>
+ * <h1> Biforce Spark Team JavaDoc</h1>
+ * <p>
+ * This class consists exclusively of a main method that interacts with
+ * utilities which interact with a logistic regression model. It has several
+ * helper methods which write output. All methods are static.
+ * </p>
+ * <p>
+ * The model returns percentage values which indicate likelihood to fail and
+ * then runs through all possible cutoff values to determine the optimal cutoff
+ * percentage.
+ * </p>
+ * <p>
+ * The model clumps trainees together in groups for each of the first few weeks
+ * by their test scores into buckets based on equi-distant percentiles. The
+ * model then calculates the odds of someone in that bucket passing or failing
+ * and then calculates the log odds taken from this tutorial.
+ * <a href="http://vassarstats.net/logreg1.html">Logistic Regression</a>.
+ * </p>
+ * <p>
+ * TODO: describe the rest of log-reg logic.
+ * </p>
+ * <p>
+ * This class is a member of the
+ * <a href="https://github.com/Dec-17-Big-Data/biforce"> Revature Biforce</a>
+ * Github project.
+ * </p>
+ * <p>
+ * For more information regarding this project see the <a href=
+ * "https://drive.google.com/open?id=1xD-x-0oX2vXWdEpoMhq0Jpu0j2gCgpjOufVh5NerJHQ">
+ * Biforce Living Document</a>.
+ * </p>
  * <h2>Used columns</h2>
  * <ul>
  * <li>_c0 = PK</li>
@@ -48,27 +62,25 @@ import com.revature.util.PartitionFinder;
  * <h2>All Columns</h2>
  * <ul>
  * <li>_c0 = 1 : row number</li>
- * <li>_c1 = 2 : A.ASSESSMENT_TYPE
- *  - 'VERBAL' = 1; 'EXAM' = 2;
- *  - 'PROJECT' = 3; 'OTHER' = 4</li>
+ * <li>_c1 = 2 : A.ASSESSMENT_TYPE - 'VERBAL' = 1; 'EXAM' = 2; - 'PROJECT' = 3;
+ * 'OTHER' = 4</li>
  * <li>_c2 = 25 : A.RAW_SCORE</li>
  * <li>_c3 = 74.00 : Q.SCORE</li>
  * <li>_c4 = 7 : A.WEEK_NUMBER 1-9 inclusive</li>
  * <li>_c5 = 20 : A.ASSESSMENT_CATEGORY</li>
  * <li>_c6 = 14 : G.TRAINER_ID</li>
  * <li>_c7 = 112511 : G.BATCH_ID</li>
- * <li>_c8 = 2 : G.SKILL_TYPE 
- *  - 'SDET' = 1; 'J2EE' = 2; 'OTHER' = 3;
- *  - 'BPM' = 4; 'NET' = 5; 'MICROSERVICES' = 6</li>
+ * <li>_c8 = 2 : G.SKILL_TYPE - 'SDET' = 1; 'J2EE' = 2; 'OTHER' = 3; - 'BPM' =
+ * 4; 'NET' = 5; 'MICROSERVICES' = 6</li>
  * <li>_c9 = 281214 : Q.TRAINEE_ID</li>
- * <li>_c10 = 2 : B.TRAINING_STATUS 
- *  - 'DROPPED'= 0; 'EMPLOYED' = 1; 'TRAINING' = 2; 
- *  - 'SIGNED' = 3; 'CONFIRMED' = 4; 'MARKETING' = 5</li>
- *  </ul>
- * @author  Mason Wegert
- * @author  Diego Gomez
- * @author  Tim Law
- * @author  Pil Ju Chun
+ * <li>_c10 = 2 : B.TRAINING_STATUS - 'DROPPED'= 0; 'EMPLOYED' = 1; 'TRAINING' =
+ * 2; - 'SIGNED' = 3; 'CONFIRMED' = 4; 'MARKETING' = 5</li>
+ * </ul>
+ * 
+ * @author Mason Wegert
+ * @author Diego Gomez
+ * @author Tim Law
+ * @author Pil Ju Chun
  **/
 
 // Read the input file in as a spark Dataset<Row> with no header, therefore the
@@ -150,12 +162,9 @@ public class Driver {
 		System.out.println("Root Mean Squared Error: " + EvaluationMetrics.testRMSE(controlRDD));
 
 		JavaPairRDD<Integer, Row> appliedResultPair = ModelApplier.applyModel(csv, modelParams);
-
-		appliedResultPair.foreach(pairTuple -> {
-			String prediction = pairTuple._2.getDouble(1)/pairTuple._2.getDouble(2) >= optimalPoint.getOptimalPercent() ? "DROP" : "PASS";
-			// ID | aggregate drop chance | sum of r^2's | week # 
-			writer.append(pairTuple._1 + "," + pairTuple._2.getDouble(1)/pairTuple._2.getDouble(2) + "," + pairTuple._2.getInt(4) + "," + prediction + "\n");
-		});
+		appliedResultPair.cache();
+		writeOutput(appliedResultPair, optimalPoint.getOptimalPercent());
+		appliedResultPair.unpersist();
 
 		// Close all the resources.
 		try {
@@ -174,7 +183,8 @@ public class Driver {
 	 * Initializes the BufferedWriter/FileWriter class combination for two writers.
 	 * One main writer with the id/prediction using mainPath, one for control data
 	 * stats using controlPath.
-	 * @param mainPath - file system location for main Writer
+	 * 
+	 * @param mainPath    - file system location for main Writer
 	 * @param controlPath - file system location for wk3Writer
 	 */
 	private static void initWriters(String mainPath, String controlPath) {
@@ -189,15 +199,19 @@ public class Driver {
 	}
 
 	/**
-	 * Prints the equation for calculating the probability of failure based on the modelParams.
-	 * Prints to the console and accuracyWriter for each of the 3 exam types (one for verbal, exam, project scores).
-	 * Test type 4 (other) has a low correlation (0.2) and negatively effects the results.
+	 * Prints the equation for calculating the probability of failure based on the
+	 * modelParams. Prints to the console and accuracyWriter for each of the 3 exam
+	 * types (one for verbal, exam, project scores). Test type 4 (other) has a low
+	 * correlation (0.2) and negatively effects the results.
+	 * 
 	 * @param modelParams
 	 */
 	private static void printModel(double[][] modelParams) {
-		for(int i = 0; i < 3; i++) {
-			String s = String.format("Exam type " + (i+1) +": partialFailChance = e^(%2.3f*score+%2.3f) / (1+e^(%2.3f*score+%2.3f), r^2 = %1.3f\n", 
-					modelParams[i][1],modelParams[i][2],modelParams[i][1],modelParams[i][2],modelParams[i][3]);
+		for (int i = 0; i < 3; i++) {
+			String s = String.format(
+					"Exam type " + (i + 1)
+							+ ": partialFailChance = e^(%2.3f*score+%2.3f) / (1+e^(%2.3f*score+%2.3f), r^2 = %1.3f\n",
+					modelParams[i][1], modelParams[i][2], modelParams[i][1], modelParams[i][2], modelParams[i][3]);
 			System.out.println(s);
 			try {
 				controlWriter.append(s);
@@ -208,8 +222,10 @@ public class Driver {
 	}
 
 	/**
-	 * Writes the controlRDD output to the accuracy writer. If a row's drop % is greater than
-	 * dropPercent it writes 'DROP', otherwise it writes 'PASS' at the end of the line.
+	 * Writes the controlRDD output to the accuracy writer. If a row's drop % is
+	 * greater than dropPercent it writes 'DROP', otherwise it writes 'PASS' at the
+	 * end of the line.
+	 * 
 	 * @param controlRDD
 	 * @param dropPercent
 	 */
@@ -231,6 +247,7 @@ public class Driver {
 			}
 		});
 	}
+
 	/**
 	 * Writes results of model applied to full csv to the output writer. If a row's
 	 * drop % is greater than dropPercent it writes 'DROP', otherwise it writes
@@ -242,8 +259,9 @@ public class Driver {
 	private static void writeOutput(JavaPairRDD<Integer, Row> appliedResultPair, double dropPercent) {
 		appliedResultPair.foreach(pairTuple -> {
 			String prediction = pairTuple._2.getDouble(1) / pairTuple._2.getDouble(2) >= dropPercent ? "DROP" : "PASS";
+			// ID | aggregate drop chance | sum of r^2's | week # 
 			writer.append(pairTuple._1 + "," + pairTuple._2.getDouble(1) / pairTuple._2.getDouble(2) + ","
-					+ pairTuple._2.getInt(3) + "," + prediction + "\n");
+					+ pairTuple._2.getInt(4) + "," + prediction + "\n");
 		});
 	}
 
@@ -259,17 +277,22 @@ public class Driver {
 	private static OptimalPoint applyControl(JavaRDD<Row> controlRDD, double accuracyDelta, int weekNum) {
 		controlRDD.cache();
 		OptimalPoint optimalPoint = ModelApplier.findOptimalPercent(controlRDD, accuracyDelta);
-		
-		writeToControl("Fail percent: " + Math.round(optimalPoint.getOptimalPercent()*10000)/10000.0 + "\nCorrect estimates: " + 
-				optimalPoint.getOptimalAccurateCount() + "\nTotal Count: " + controlRDD.count() + "\nAccuracy: " + 
-				(double) optimalPoint.getOptimalAccurateCount()/(double)controlRDD.count() + "\n\n", weekNum);
+
+		writeToControl(
+				"Fail percent: " + Math.round(optimalPoint.getOptimalPercent() * 10000) / 10000.0
+						+ "\nCorrect estimates: " + optimalPoint.getOptimalAccurateCount() + "\nTotal Count: "
+						+ controlRDD.count() + "\nAccuracy: "
+						+ (double) optimalPoint.getOptimalAccurateCount() / (double) controlRDD.count() + "\n\n",
+				weekNum);
 
 		controlRDD.unpersist();
 		return optimalPoint;
 	}
 
 	/**
-	 * a simple writer class to the accuracyWriter and console, outString gets written/appended.
+	 * a simple writer class to the accuracyWriter and console, outString gets
+	 * written/appended.
+	 * 
 	 * @param outString
 	 */
 	private static void writeToControl(String outString, int weekNum) {
