a204ab6e54557e778dc5ce042492d0a7d6f462f7
LTCHIPS <ltchips994@gmail.com>
2019-03-28 16:25:12 -0400

    added spark code to communicate with hive
    Keep in mind that as of this commit that it will ONLY WORK IN THE
    CLOUDERA ENVIRONMENT. Things will need to be changed when it comes time to
    deploy spark jobs

--- biforce/spark/TrainerAnalysis/src/main/java/com/revature/TrainerAnalysis/DriverClass.java
+++ biforce/spark/TrainerAnalysis/src/main/java/com/revature/TrainerAnalysis/DriverClass.java
@@ -1,5 +1,6 @@
 package com.revature.TrainerAnalysis;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,23 +10,44 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.SparkSession;
 
 import com.revature.Util.*;
 
+//how to get this to read from hive
+
+//1. Copy core-site.xml, hdfs-site.xml, and hive-site.xml to /etc/spark/conf
+//2. Edit spark-env.sh to include 'export HADOOP_CONF_DIR=/etc/hadoop/conf/' (or whereever hadoop config is)
+
+//Currently, its set up to work in the cloudera VM environment, so things will need to be reconfigured for when we deploy things!
+		
+//NOTE: You will need Spark 2.4.0 in order to run this. Remember that Spark is NOT backwards compatible!
+
 public class DriverClass {
 	public static void main(String[] args) {
-		final String inputpath = args[0];
-		final String outputpath = args[1];
 		
-		SparkConf conf = new SparkConf().setAppName("TrainerAnalysis");
-		JavaSparkContext context = new JavaSparkContext(conf);
+		//Be sure to have the data in hive before you try and run this
+		//See readme under ETL for how to do that
 		
-		new xxx.write.format("csv").save("s3://revature-analytics-dev");
-		new yyy.write.format("csv").save("s3://revature-analytics-dev");
-		new zzz.write.format("csv").save("s3://revature-analytics-dev");
-		new aaa.write.format("csv").save("s3://revature-analytics-dev");
-		new hhh.write.format("csv").save("s3://revature-analytics-dev");
+		SparkSession spark = SparkSession
+		  .builder()
+		  .master("local")
+		  .appName("Java Spark Hive Job") //TODO: PLEASE GIVE ME A MORE MEANINGFUL NAME
+		  .config("spark.sql.warehouse.dir", "/user/hive/warehouse")
+		  .config("hive.metastore.uris", "thrift://localhost:9083")
+		  .enableHiveSupport()
+		  .getOrCreate();
+		
+		SQLContext sparkSQL = spark.sqlContext();
+		
+		//The following is just a test, please delete me later
+		
+		sparkSQL.sql("use biforce_staging");
+		
+		Dataset<Row> stuff = sparkSQL.sql("SELECT * FROM caliber_address");
+		
+		System.out.println(stuff.count());
 		
 	}
 }
