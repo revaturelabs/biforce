adaf0c736fb9834f5c55e17fe1d2ac60077ab975
Luke Davis <lukedavis3@gmail.com>
2019-03-31 23:12:29 -0400

    Made significant changes/fixes to the grade jobs
    I added more input files to both BatchPlusGradeSubmission.java and
    SpecificWeeksSubmittedTable.java, among other small changes/fixes.

--- biforce/spark/TrainerAnalysis/src/main/java/com/revature/Util/BatchPlusGradeSubmission.java
+++ biforce/spark/TrainerAnalysis/src/main/java/com/revature/Util/BatchPlusGradeSubmission.java
@@ -12,29 +12,24 @@ import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 
 public class BatchPlusGradeSubmission {
-	public static void calcGradeSubmission(JavaSparkContext context, SparkSession session, String input, String output) {
+	public static void calcGradeSubmission(JavaSparkContext context, SparkSession session, String input_batch, String input_assessment, String input_note, String output) {
 		//Constructs a schema in the metastore for Spark to load data into and read from
-		StructField batch_id = DataTypes.createStructField("Batch_ID", DataTypes.DoubleType, true);
+		StructField batch_id = DataTypes.createStructField("batch_id", DataTypes.IntegerType, true);
         StructField borderline_grade_threshold = DataTypes.createStructField("borderline_grade_threshold", DataTypes.DoubleType, true);
-        StructField end_date = DataTypes.createStructField("end_date", DataTypes.DoubleType, true);
-        StructField good_grade_threshold = DataTypes.createStructField("good_grade_threshold", DataTypes.StringType, true);
-        StructField location = DataTypes.createStructField("location", DataTypes.StringType, true);
-        StructField skill_type = DataTypes.createStructField("skill_type", DataTypes.DoubleType, true);
-        StructField start_date = DataTypes.createStructField("start_date", DataTypes.DoubleType, true);
+		StructField end_date = DataTypes.createStructField("end_date", DataTypes.DateType, true);
+        StructField good_grade_threshold = DataTypes.createStructField("good_grade_threshold", DataTypes.DoubleType, true);
+		StructField location = DataTypes.createStructField("location", DataTypes.StringType, true);
+        StructField skill_type = DataTypes.createStructField("skill_type", DataTypes.StringType, true);
+		StructField start_date = DataTypes.createStructField("start_date", DataTypes.DateType, true);
         StructField training_name = DataTypes.createStructField("training_name", DataTypes.StringType, true);
-        StructField training_type = DataTypes.createStructField("training_type", DataTypes.StringType, true);
-        StructField number_of_weeks = DataTypes.createStructField("number_of_weeks", DataTypes.StringType, true);
-		StructField co_trainer_id = DataTypes.createStructField("co_trainer_id", DataTypes.StringType, true);
-		StructField trainer_id = DataTypes.createStructField("trainer_id", DataTypes.StringType, true);
-		StructField resource_id = DataTypes.createStructField("resource_id", DataTypes.StringType, true);
-		StructField address_id = DataTypes.createStructField("address_id", DataTypes.StringType, true);
-		StructField graded_weeks = DataTypes.createStructField("graded_weeks", DataTypes.StringType, true);
-		StructField graded_weeks_exam = DataTypes.createStructField("graded_weeks_exam", DataTypes.StringType, true);
-		StructField graded_weeks_verbal = DataTypes.createStructField("graded_weeks_verbal", DataTypes.StringType, true);
-		StructField graded_weeks_target = DataTypes.createStructField("graded_weeks_target", DataTypes.StringType, true);
-        
+		StructField training_type = DataTypes.createStructField("training_type", DataTypes.StringType, true);
+        StructField number_of_weeks = DataTypes.createStructField("number_of_weeks", DataTypes.IntegerType, true);
+        StructField co_trainer_id = DataTypes.createStructField("co_trainer_id", DataTypes.IntegerType, true);
+        StructField trainer_id = DataTypes.createStructField("trainer_id", DataTypes.IntegerType, true);
+        StructField resource_id = DataTypes.createStructField("resource_id", DataTypes.StringType, true);
+        StructField address_id = DataTypes.createStructField("address_id", DataTypes.IntegerType, true);
+        StructField graded_weeks = DataTypes.createStructField("graded_weeks", DataTypes.IntegerType, true);
         List<StructField> fields = new ArrayList<StructField>();
-        
         fields.add(batch_id);
         fields.add(borderline_grade_threshold);
         fields.add(end_date);
@@ -45,20 +40,56 @@ public class BatchPlusGradeSubmission {
         fields.add(training_name);
         fields.add(training_type);
         fields.add(number_of_weeks);
-		fields.add(co_trainer_id);
-		fields.add(trainer_id);
-		fields.add(resource_id);
-		fields.add(address_id);
-		fields.add(graded_weeks);
-		fields.add(graded_weeks_exam);
-		fields.add(graded_weeks_verbal);
-		fields.add(graded_weeks_target);
-        
-        StructType schema = DataTypes.createStructType(fields);
+        fields.add(co_trainer_id);
+        fields.add(trainer_id);
+        fields.add(resource_id);
+        fields.add(address_id);
+        fields.add(graded_weeks);
+        StructType schema = DataTypes.createStructType(fields);		
+		Dataset<Row> data_batch = session.sqlContext().read().format("csv").option("delimiter", "~").option("header", "false").schema(schema).load(input_batch);
+		data_batch.createOrReplaceTempView("caliber_batch");
 		
-		Dataset<Row> data = session.sqlContext().read().format("csv").option("delimiter", "~").option("header", "false").schema(schema).load(input);
+		StructField assessment_id = DataTypes.createStructField("assessment_id", DataTypes.IntegerType, true);
+        StructField raw_score = DataTypes.createStructField("raw_score", DataTypes.DoubleType, true);
+		StructField assessment_title = DataTypes.createStructField("assessment_title", DataTypes.StringType, true);
+        StructField assessment_type = DataTypes.createStructField("assessment_type", DataTypes.StringType, true);
+		StructField week_number = DataTypes.createStructField("week_number", DataTypes.IntegerType, true);
+        StructField batch_id2 = DataTypes.createStructField("batch_id", DataTypes.IntegerType, true);
+		StructField assessment_category = DataTypes.createStructField("assessment_category", DataTypes.StringType, true);
+        fields = new ArrayList<StructField>();
+        fields.add(assessment_id);
+        fields.add(raw_score);
+        fields.add(assessment_title);
+        fields.add(assessment_type);
+        fields.add(week_number);
+        fields.add(batch_id2);
+        fields.add(assessment_category);
+        schema = DataTypes.createStructType(fields);
+		Dataset<Row> data_assessment = session.sqlContext().read().format("csv").option("delimiter", "~").option("header", "false").schema(schema).load(input_assessment);
+		data_assessment.createOrReplaceTempView("caliber_assessment");
 		
-		data.createOrReplaceTempView("BatchPlusGradeSubmission");
+		StructField note_id = DataTypes.createStructField("note_id", DataTypes.IntegerType, true);
+        StructField note_content = DataTypes.createStructField("note_content", DataTypes.DoubleType, true);
+		StructField max_visibility = DataTypes.createStructField("max_visibility", DataTypes.StringType, true);
+        StructField is_qc_feedback = DataTypes.createStructField("is_qc_feedback", DataTypes.StringType, true);
+		StructField qc_status = DataTypes.createStructField("qc_status", DataTypes.IntegerType, true);
+        StructField note_type = DataTypes.createStructField("note_type", DataTypes.IntegerType, true);
+		StructField week_number = DataTypes.createStructField("week_number", DataTypes.StringType, true);
+		StructField batch_id3 = DataTypes.createStructField("batch_id", DataTypes.StringType, true);
+		StructField trainee_id = DataTypes.createStructField("trainee_id", DataTypes.StringType, true);
+        fields = new ArrayList<StructField>();
+        fields.add(note_id);
+        fields.add(note_content);
+        fields.add(max_visibility);
+        fields.add(is_qc_feedback);
+        fields.add(qc_status);
+        fields.add(note_type);
+        fields.add(week_number);
+		fields.add(batch_id3);
+		fields.add(trainee_id);
+        schema = DataTypes.createStructType(fields);
+		Dataset<Row> data_assessment = session.sqlContext().read().format("csv").option("delimiter", "~").option("header", "false").schema(schema).load(input_note);
+		data_assessment.createOrReplaceTempView("caliber_note");
 		
         //Executes SQL query to aggregate data
 		Dataset<Row> BatchPlusGradeSubmissionTemp = session.sqlContext().sql("CREATE VIEW BatchPlusGradeSubmissionTemp  AS\r\n" + 
